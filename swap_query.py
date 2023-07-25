068de4e627fbde7d0c980f872e2e28626f93cf06
Aishlia <nai.julia@gmail.com>
2023-07-25 15:52:16 -0700

    Add script for persisting transaction data

--- swap_query.py
+++ swap_query.py
@@ -5,47 +5,82 @@ import sqlite3
 
 import time
 
-# CREATE TABLE swap_transactions (
-#     id TEXT,
-#     amountUSD REAL,
-#     timestamp TIMESTAMP,
-#     wallet_address TEXT,
-#     token0_abbr TEXT,
-#     token0_name TEXT,
-#     amount0 REAL,
-#     token0_feesUSD REAL,
-#     token1_abbr TEXT,
-#     token1_name TEXT
-#     amount1 REAL,
-#     token1_feesUSD REAL,
-#     UNIQUE(id)
-# );
+# Persisting transaction data
+def persist_swap_transactions(con, cur):
+    # CREATE TABLE swap_transactions (
+    #     id TEXT,
+    #     amountUSD REAL,
+    #     timestamp TIMESTAMP,
+    #     wallet_address TEXT,
+    #     token0_abbr TEXT,
+    #     token0_name TEXT,
+    #     amount0 REAL,
+    #     token0_feesUSD REAL,
+    #     token1_abbr TEXT,
+    #     token1_name TEXT
+    #     amount1 REAL,
+    #     token1_feesUSD REAL,
+    #     UNIQUE(id)
+    # );
 
-now = time.time()
+    query = """{
+    swaps {
+    amount0
+    amount1
+    amountUSD
+    id
+    origin
+    timestamp
+    token1 {
+        name
+        symbol
+        feesUSD
+    }
+    token0 {
+        symbol
+        name
+        feesUSD
+    }
+    }
+    }"""
 
-con = sqlite3.connect("test.db")
-cur = con.cursor()
+    url = 'https://api.thegraph.com/subgraphs/name/nick8319/uniswap-v3-harmony'
+    r = requests.post(url, json={'query': query})
+    response_json = r.json()['data']['swaps']
+    for txn in response_json:
+        id = txn['id']
+        amountUSD = txn['amountUSD']
+        timestamp = txn['timestamp']
+        wallet_address = txn['origin']
 
-query = """{
-  factories(first: 5) {
-    totalVolumeUSD
-  }
-}"""
+        token0_abbr = txn['token0']['symbol']
+        token0_name = txn['token0']['name']
+        amount0 = txn['amount0']
+        token0_feesUSD = txn['token0']['feesUSD']
 
-url = 'https://api.thegraph.com/subgraphs/name/nick8319/uniswap-v3-harmony'
-r = requests.post(url, json={'query': query})
-response = r.text 
-response = response[response.find('VolumeUSD":"')+12:-6]
-response = int(response.split('.')[0])
+        token1_abbr = txn['token1']['symbol']
+        token1_name = txn['token1']['name']
+        amount1 = txn['amount1']
+        token1_feesUSD = txn['token1']['feesUSD']
 
-insertQuery = """Insert INTO tvltest
-    VALUES (?,?);"""
+        insertQuery = """Insert INTO swap_transactions (id, amountUSD, timestamp, wallet_address, token0_abbr, token0_name, amount0, token0_feesUSD, token1_abbr, token1_name, amount1, token1_feesUSD)
+        VALUES (?,?,?,?,?,?,?,?,?,?,?,?);"""
 
-cur.execute(insertQuery, (response, now))
-con.commit()
-cur.close()
-con.close()
+        try:
+            cur.execute(insertQuery, (id, amountUSD, timestamp, wallet_address, token0_abbr,
+                        token0_name, amount0, token0_feesUSD, token1_abbr, token1_name, amount1, token1_feesUSD))
+        except:
+            pass
 
-# create table tvltest (
-#    ...> TVLUSD INT,
-#    ...> datetime TIMESTAMP);
+    con.commit()
+    cur.close()
+    con.close()
+
+
+if __name__ == "__main__":
+    db_name = 'swap_data.db'
+
+    con = sqlite3.connect(db_name)
+    cur = con.cursor()
+
+    persist_swap_transactions(con, cur)
