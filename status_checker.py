bccd7fe0b5516c5a5672b3969ec10f5e3a79211b
Aishlia <nai.julia@gmail.com>
2023-07-27 01:19:37 -0700

    Added barebones swap.country status checker

---
+++ status_checker.py
@@ -0,0 +1,71 @@
+import requests
+import time
+import sqlite3
+import socket
+import os
+
+# CREATE TABLE swap_status (
+#     check_datetime DATETIME,
+#     url TEXT,
+#     response_code INTEGER,
+#     response_time REAL,
+#     req_log TEXT,
+#     socket_ip TEXT,
+#     socket_log TEXT,
+#     ping_resp INT
+# );
+
+# 99999 is error
+
+def check_site_status(con, cur, url, max_retries=3):
+    retry = 0
+
+    try:
+        socket_ip = socket.gethostbyname("url")
+        socket_log = ''
+    except socket.gaierror as e:
+        socket_ip = ''
+        socket_log = str(e)
+
+    try:
+        ping_resp = int(os.system("ping -c 1 " + url))
+    except Exception as e:
+        ping_resp = str(e)
+
+    while retry <= max_retries:
+        try:
+            response = requests.get(url)
+            response_code = response.status_code
+            response_time = response.elapsed.total_seconds()
+            
+            insertQuery = """Insert INTO swap_status (check_datetime, url, response_code, response_time, socket_ip, socket_log, ping_resp)
+            VALUES (?,?,?,?,?,?,?);"""
+
+            cur.execute(insertQuery, (time.time(), url, response_code, response_time, socket_ip, socket_log, ping_resp))
+
+            break
+        except requests.exceptions.RequestException as e:
+            retry += 1
+            if retry <= max_retries:
+                insertQuery = """Insert INTO swap_status (check_datetime, url, response_code, req_log, socket_ip, socket_log, ping_resp)
+                VALUES (?,?,?,?,?,?,?);"""
+                cur.execute(insertQuery, (time.time(), url, 99999, str(e), socket_ip, socket_log, ping_resp))
+                time.sleep(2**retry)
+            else:
+                break
+            
+
+if __name__ == "__main__":
+    site_url = ["https://swap.country", "swap.country"]
+
+    db_name = 'swap_status.db'
+
+    con = sqlite3.connect(db_name)
+    cur = con.cursor()
+
+    for url in site_url:
+        check_site_status(con, cur, url)
+
+    con.commit()
+    cur.close()
+    con.close()
